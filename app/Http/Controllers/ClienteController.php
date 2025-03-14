5f8b22d7fa986a7345fa9153c4d9bd177b28ab59
MauricioH <mauricioh12345678912@gmail.com>
2025-03-14 01:40:46 -0500

    comentarios en cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -8,18 +8,25 @@ use App\Models\Cliente;
 
 class ClienteController extends Controller
 {
+    // este metodo es para listar todos los clientes
     public function listarTodosLosClientes(){
         $clientes = DB::table('clientes')->get();
         return response()->json($clientes);
     }
 
+    
+    // este metodo es para crear un cliente
     public function agregarCliente(Request $request){
-        $request->validate([
+
+        // validacion de los datos enviados por el cliente para la creacion del cliente
+        $request->validate([ 
             'nombre' => 'required|string|max:255',
             'celular' => 'nullable|string|max:15',
             'correo' => 'required|email',
             'direccion' => 'nullable|string|max:255',
         ]);
+
+        // creacion del cliente
 
         $cliente = Cliente::create([
             'nombre' => $request->nombre,
@@ -27,6 +34,8 @@ class ClienteController extends Controller
             'celular' => $request->celular,
             'direccion' => $request->direccion
         ]);
+
+        // retirno de una respuesta json al cliente 
         return response()->json([
             'message' => 'Clente creado correctamente',
             'cliente' => $cliente
