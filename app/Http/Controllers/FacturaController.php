5aa5225e6ffb373dab83001388c2185e6fbd0cb0
MauricioH <mauricioh12345678912@gmail.com>
2025-03-22 18:41:58 -0500

    creacion del metodo de crear factura, detalle de factura y tipo de pago en el servicio de factura

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -3,13 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\DetallePago;
+use App\Services\FacturaService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Factura;
 use App\Models\DetalleFactura;
 use Exception;
+
 class FacturaController extends Controller
 {
+
+    protected  $facturaService;
+
+    public function __construct(FacturaService $facturaService)
+    {
+        $this->facturaService = $facturaService;
+    }
+
+
+    protected function validacion($request)
+    {
+        // Validacion de lso datos de la factura y sus detalles
+        $request->validate([
+            // datos de la factura
+            'cliente_id' => 'required|integer',
+            'fecha' => 'required|date',
+            //aparatir de aqui son los datos del detalled e la factura
+            'productos' => 'required|array|min:1',
+            'productos.*.producto_id' => 'required|integer',
+            'productos.*.cantidad' => 'required|integer|min:1',
+            'productos.*.precio_unitario' => 'required|numeric|min:0',
+            'tipo_pago_id' => 'required|integer',
+            'estado' => 'required|boolean'
+        ]);
+    }
 
     public  function listarFacturas(Request $request)
     {
@@ -18,61 +45,23 @@ class FacturaController extends Controller
         return response()->json($facturas, 200);
     }
 
+
     public function crearFactura(Request $request)
     {
+        $this->validacion($request);
 
-        try {
-            // Validacion de lso datos de la factura y sus detalles
-            $request->validate([
-                // datos de la factura
-                'cliente_id' => 'required|integer',
-                'fecha' => 'required|date',
-                //aparatir de aqui son los datos del detalled e la factura
-                'productos' => 'required|array|min:1',
-                'productos.*.producto_id' => 'required|integer',
-                'productos.*.cantidad' => 'required|integer|min:1',
-                'productos.*.precio_unitario' => 'required|numeric|min:0',
-                'tipo_pago_id' => 'required|integer',
-                'estado' => 'required|boolean'
-            ]);
-
-            $factura = Factura::create([
-                'cliente_id' => $request->cliente_id,
-                'fecha' => $request->fecha,
-            ]);
-
-            $detalleFactura = [];
-
-            foreach($request->productos as $producto){
-                $detalleFactura[] = DetalleFactura::create([
-                    'factura_id' => $factura->id,
-                    'producto_id' => $producto['producto_id'],
-                    'cantidad' => $producto['cantidad'],
-                    'precio_unitario' => $producto['precio_unitario']
-                ]);
-            }
-
-            $detallePago = DetallePago::create([
-                'factura_id' => $factura->id,
-                'tipo_pago_id' => $request->tipo_pago_id,
-                'estado' => $request->estado
-            ]);
-
-            return response()->json([
-                'message' => 'Factura creada correctamente',
-                'factura' => $factura,
-                'detalleFactura' => $detalleFactura,
-                'tipo_pago' => $detallePago
-            ], 201);
-        } catch (Exception $e) {
-            return response()->json([
-                'message' => 'Error al crear la factura',
-                'error' => $e->getMessage()
-            ], 500);
+        $resultado = $this->facturaService->crearFactura($request);
+        if ($resultado["error"]) {
+            return response()->json($resultado, 400);
         }
+
+        return response()->json($resultado, 200);
     }
 
-    public function eliminarFactura($id){
+
+
+    public function eliminarFactura($id)
+    {
 
         try {
             $factura = Factura::find($id);
@@ -85,8 +74,6 @@ class FacturaController extends Controller
                 'message' => 'Error al eliminar la factura',
                 'error' => $e->getMessage()
             ], 500);
-            
         }
-
     }
 }
--- app/Services/FacturaService.php
+++ app/Services/FacturaService.php
@@ -2,6 +2,64 @@
 
 namespace App\Services;
 
-class FacturaService{
-    
+use App\Models\Factura;
+use App\Models\DetalleFactura;
+use App\Models\DetallePago;
+use Exception;
+
+class FacturaService
+{
+
+    public function crearDetalleFactura($factura, $request)
+    {
+        $detalleFactura = [];
+
+        foreach ($request->productos as $producto) {
+            $detalleFactura[] = DetalleFactura::create([
+                'factura_id' => $factura->id,
+                'producto_id' => $producto['producto_id'],
+                'cantidad' => $producto['cantidad'],
+                'precio_unitario' => $producto['precio_unitario']
+            ]);
+        }
+
+        return $detalleFactura;
+    }
+
+    public function CrearDetallePago($factura, $request){
+        $detallePago = DetallePago::create([
+            'factura_id' => $factura->id,
+            'tipo_pago_id' => $request->tipo_pago_id,
+            'estado' => $request->estado
+        ]);
+
+        return $detallePago;
+    }
+
+    public function crearFactura($request)
+    {
+        try {
+            $factura = Factura::create([
+                'cliente_id' => $request->cliente_id,
+                'fecha' => $request->fecha,
+            ]);
+
+            $detalleFactura = $this->crearDetalleFactura($factura, $request);
+
+            $detallePago = $this->CrearDetallePago($factura, $request);
+
+            return[
+                'error' => false,
+                'message' => 'Factura creada correctamente',
+                'factura' => $factura,
+                'detalleFactura' => $detalleFactura,
+                'tipo_pago' => $detallePago
+            ];
+        } catch (Exception $e) {
+            return [
+                'error' => true,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
 }
