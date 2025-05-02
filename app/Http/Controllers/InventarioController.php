4f65318781b2e831c6527c61570cb0585a66928a
MauricioH <mauricioh12345678912@gmail.com>
2025-05-01 19:42:09 -0500

    modificacion del metodo para obetener los productos del inventario con paginacion

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -7,14 +7,18 @@ use Illuminate\Support\Facades\DB;
 
 class InventarioController extends Controller
 {
-    public function listarProductosInventario(){
+    public function listarProductosInventario(Request $request){
         $datos = DB::table('inventarios as in')
         ->select([
+            'pr.id',
             'pr.nombre_producto',
+            'pb.nombre_proveedor',
             'in.cantidad'
+
         ])
         ->join('productos as pr', 'pr.id', '=', 'in.producto_id')
-        ->get();
+        ->join('proveedors as pb', 'pb.id', '=', 'pr.proveedor_id')
+        ->paginate(10);
 
         return response()->json($datos);
     }
